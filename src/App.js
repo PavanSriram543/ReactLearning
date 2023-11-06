529ed0bc01201e883559ff77250cf18d50d4b43f
Pavan Kumar <pavankumar@Pavans-MacBook-Air.local>
2023-11-06 10:39:46 +0530

    currency converter added and custom hooks too

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import PasswordGenerator from "./PasswordGenerator";
+import Currency from "./projects/currencyProject";
 
 function App() {
   return (
     <div className="App">
       <PasswordGenerator />
+      <Currency />
     </div>
   );
 }
